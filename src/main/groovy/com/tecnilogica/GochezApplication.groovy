c47844db1b8f922a2e0e028e498dbb481a83f8e6
Jose Juan Montiel <josejuan.montiel@tecnilogica.com>
2016-03-22 16:34:41 +0100

    Ejemplo de aplicacion spring-boot

---
+++ src/main/groovy/com/tecnilogica/GochezApplication.groovy
@@ -0,0 +1,12 @@
+package com.tecnilogica
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GochezApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run GochezApplication, args
+	}
+}
